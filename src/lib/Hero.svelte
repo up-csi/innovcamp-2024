e9a8a816447d4d0995e94290819630fe04054cb5
Hans Lorico <hqlorico@up.edu.ph>
2024-05-10 16:04:39 +0800

    fix: remove skeleton overflow restriction

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import bg_src from "$lib/assets/ic-svelte-bg.jpg";
+    import logo_ic from "$lib/assets/main-logo-ic.png"
+</script>
+
+
+<div class="relative">
+    <div class="h-screen flex items-center justify-center">
+        <img alt="svelte_surge_logo" src={logo_ic} class="absolute h-44 w-max"/>
+    </div>
+    <img alt="svelte_surge_bg" src={bg_src} class="object-cover h-screen w-full -z-20"/>
+    <div class="flex justify-center items-center h-full">
+        <h1 class="text-4xl">Page</h1>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Hero from '$lib/Hero.svelte';
+</script>
+
+<div class="flex flex-col">
+    <div class="h-full">
+        <Hero />
+    </div>
+
+    <div class="h-screen bg-stone-300">
+        TESTING
+        <div class="h-46 bg-slate-300">
+            Hello
+        </div>
+    </div>
+</div>
