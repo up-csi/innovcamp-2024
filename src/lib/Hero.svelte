817cf327b65dd9ccab3ae556bc4b5f26b0b559a1
Hans Lorico <hqlorico@up.edu.ph>
2024-05-10 16:25:47 +0800

    feat: include hero elements to above the fold section

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
     import bg_src from "$lib/assets/ic-svelte-bg.jpg";
     import logo_ic from "$lib/assets/main-logo-ic.png"
+    import cs_logos from "$lib/assets/ic-cs-Logos.png"
 </script>
 
 
 <div class="relative">
     <div class="h-screen flex items-center justify-center">
-        <img alt="svelte_surge_logo" src={logo_ic} class="absolute h-44 w-max"/>
+        <img alt="svelte_surge_logo" src={cs_logos} class="absolute h-8 sm:h-10 md:h-20 w-max top-10"/>
+        <img alt="svelte_surge_logo" src={logo_ic} class="absolute h-44 sm:h-60 md:h-96 w-max"/>
+        <p class="absolute bottom-20 text-center text-xs sm:text-md md:text-xl text-white">
+            MAY 15, 2024<br>
+            6:00 PM - 9:00 PM<br>
+            Accenture Room<br>
+            ALUMNI ENGINEERS’ CENTENNIAL HALL<br>
+        </p>
+        <img alt="svelte_surge_bg" src={bg_src} class="absolute object-cover h-screen w-full -z-20"/>
     </div>
-    <img alt="svelte_surge_bg" src={bg_src} class="object-cover h-screen w-full -z-20"/>
+    
     <div class="flex justify-center items-center h-full">
         <h1 class="text-4xl">Page</h1>
     </div>
