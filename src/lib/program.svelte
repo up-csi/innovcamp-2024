0acac3b31abf79cb691c54aaa517558c607154cd
Sidney Lance Fernando <scfernando@up.edu.ph>
2024-05-11 17:57:59 +0800

    feat: make PM consistent in program

--- src/lib/program.svelte
+++ src/lib/program.svelte
@@ -2,10 +2,10 @@
     const tableArr = [
         { event: 'Doors Open', time: '05:30 PM' },
         { event: 'Introduction: <Speaker>', time: '06:00 PM - 06:15 PM' },
-        { event: 'Workshop 1: <Title>', time: '06:15 - 07:15 PM' },
-        { event: 'Company Talk 1: <Title>', time: '07:15 - 07:30 PM' },
-        { event: 'Workshop 2: <Title>', time: '07:30 - 08:30 PM' },
-        { event: 'Company Talk 2: <Title>', time: '08:30 - 08:45 PM' },
+        { event: 'Workshop 1: <Title>', time: '06:15 PM - 07:15 PM' },
+        { event: 'Company Talk 1: <Title>', time: '07:15 PM - 07:30 PM' },
+        { event: 'Workshop 2: <Title>', time: '07:30 PM - 08:30 PM' },
+        { event: 'Company Talk 2: <Title>', time: '08:30 PM - 08:45 PM' },
         { event: 'Closing: <Speaker>', time: '08:45 PM - 09:00 PM' },
         { event: 'End of Program', time: '09:00 PM' },
     ];
