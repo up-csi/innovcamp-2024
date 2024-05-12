b9fe4c1a4220144c122ee1cf4680715b8a24df02
mikelReinanArcardo <mikeljuly28@gmail.com>
2024-05-12 20:12:07 +0800

    chore lint js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import About from '$lib/about-event.svelte';
+    import AboutCsi from '$lib/About-csi.svelte';
     import Hero from '$lib/Hero.svelte';
     import Program from '$lib/program.svelte';
     import Speaker from '$lib/Speaker.svelte';
-    import AboutCsi from '$lib/About-csi.svelte';
 </script>
 
 <div class="flex flex-col">
