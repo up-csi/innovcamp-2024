70681b7cda11c28ed74c9e36243c63f22ddc3087
mikelReinanArcardo <mikeljuly28@gmail.com>
2024-05-12 20:04:40 +0800

    build: merge main into about_csi branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import About from '$lib/about-event.svelte';
     import Hero from '$lib/Hero.svelte';
     import Program from '$lib/program.svelte';
     import Speaker from '$lib/Speaker.svelte';
@@ -8,6 +9,10 @@
 <div class="flex flex-col">
     <div class="">
         <Hero />
+    </div>
+
+    <div class="" id="about">
+        <About />
     </div>
 
     <div class="" id="schedule">
