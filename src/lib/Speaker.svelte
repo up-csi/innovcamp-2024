e8c4fffeeb3832a3506c04693e7fe6d020524691
mikelReinanArcardo <mikeljuly28@gmail.com>
2024-05-13 16:46:18 +0800

    style: bold important details of speaker

--- src/lib/Speaker.svelte
+++ src/lib/Speaker.svelte
@@ -30,15 +30,15 @@
             <ul class="list-disc">
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                     <Lightning />
-                    <p>Full-stack web developer with 8 years of experience</p>
+                    <p><b class="font-bold">Full-stack web developer</b> with 8 years of experience</p>
                 </li>
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                     <Lightning />
-                    <p>NDSL Researcher under Dr. Wilson M. Tan (uses Svelte for CS 199)</p>
+                    <p><b class="font-bold">NDSL Researcher</b> under Dr. Wilson M. Tan (uses Svelte for CS 199)</p>
                 </li>
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                     <Lightning />
-                    <p>Trusted Author at DEV</p>
+                    <p class="font-bold">Trusted Author at DEV</p>
                 </li>
                 <div class="flex items-center justify-center py-2 sm:gap-2">
                     <span class="variant-filled badge rounded-lg bg-[#FF6534] text-center"
@@ -60,7 +60,7 @@
                 </li>
                 <li class="flex items-center space-x-3 rtl:space-x-reverse">
                     <Lightning />
-                    <p>Also known as a legend of the #acad-inquiries</p>
+                    <p>Also known as a legend of the <b class="font-bold">#acad-inquiries</b></p>
                 </li>
             </ul>
 
