d57e33a67277792b1c1669c35bb18d73d71a2708
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-10-25 06:16:31 +0800

    feat: add gallery section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
     import About from '$lib/about-event.svelte';
     import AboutCsi from '$lib/About-csi.svelte';
+    import Gallery from '$lib/Gallery.svelte';
     import Hero from '$lib/Hero.svelte';
     import Program from '$lib/program.svelte';
     import Speaker from '$lib/Speaker.svelte';
 </script>
 
 <div class="flex flex-col overflow-hidden">
-    <div class="">
+    <div>
         <Hero />
     </div>
 
-    <div class="" id="about">
+    <div id="about">
         <About />
     </div>
 
-    <div class="" id="schedule">
+    <div id="schedule">
         <Program />
     </div>
 
-    <div class="" id="speakers">
+    <div id="speakers">
         <Speaker />
     </div>
 
-    <div class="" id="about-csi">
+    <div id="gallery">
+        <Gallery />
+    </div>
+
+    <div id="about-csi">
         <AboutCsi />
     </div>
 </div>
